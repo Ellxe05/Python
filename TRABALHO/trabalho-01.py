6192441bbbae902237459ad5bed431b839beea11
Gabrielle <142362674+Ellxe05@users.noreply.github.com>
2024-04-11 18:00:21 -0300

    Create trabalho-01.py

---
+++ TRABALHO/trabalho-01.py
@@ -0,0 +1,14 @@
+#QUESTÃO 01
+print("Bem-vindo a Loja da Gabrielle Silva!")
+
+#VALOR UNITÁRIO
+valor_unitario = int(input('Informe o valor do produto: '))
+
+#QUANTIDADE DO PRODUTO
+quant_produto = int(input('Informe a quantidade do produto: '))
+
+#COM DESCONTO
+print(f"Valor COM o desconto: ")
+
+#SEM DESCONTO
+print(f"Valor SEM o desconto: ")
