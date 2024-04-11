60fe19ee8c7ea7771d58f8efad35d3d008562c1e
Gabrielle <142362674+Ellxe05@users.noreply.github.com>
2024-04-11 15:00:39 -0300

    aula 02

---
+++ Aula 02/operaçoes-aritmeticas.py
@@ -0,0 +1,11 @@
+print(2 + 3)
+print("2 + 3")
+print('2' + '3')
+
+print('Olá, ' + 'mundo!')
+print('Olá,','mundo!')
+
+print('O resultado de 2 + 3 é:', 2 + 3)
+
+print(10*(5 + 7)/4)
+print(10*(5 + 7/4))
