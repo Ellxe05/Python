e26aa6cc346bf23d9e18cec0a71a4c1b8c536969
Gabrielle <142362674+Ellxe05@users.noreply.github.com>
2024-04-11 15:47:46 -0300

    variaveis, dados e seus tipos

---
+++ Aula 02/variaveis,dados-e-seus-tipos.py
@@ -0,0 +1,23 @@
+nota = 8.5
+disciplina = 'Lógica de Programação e Algorítmos'
+
+print(nota)
+print(disciplina)
+print('Disciplina: ', disciplina, '. Nota: ', nota)
+
+#VARIAVEL LOGICA
+a = 1 #a recebe 1
+b = 5 #b recebe 5
+
+resposta = a == b
+print(resposta)
+
+resposta = a != b
+print(resposta)
+
+frase = 'Hello, world!'
+print(frase)
+
+print(frase[0])
+print(frase[2])
+print(frase[6])
