9f37129acc3c5780cdf08050286c22a8eda72c04
Gabrielle <142362674+Ellxe05@users.noreply.github.com>
2024-04-12 15:18:29 -0300

    cond. simples e composta

---
+++ Aula 03/ex-condicional-simples-e-composta.py
@@ -0,0 +1,24 @@
+#CONDICIONAL SIMPLES
+valor_a = 10
+valor_b = 10
+
+if (valor_a > valor_b):
+    print(f'O número {valor_a} é maior que o número {valor_b}.')
+if (valor_a < valor_b):
+    print(f'O número {valor_a} é menor que o número {valor_b}.')
+if (valor_a == valor_b):
+    print('Valores iguais.')
+
+#CONDICIONAL COMPOSTA
+if (valor_a > valor_b):
+    print(f'O número {valor_a} é maior que o número {valor_b}.')
+else:
+    print(f'O número {valor_a} é menor que o número {valor_b}.')
+if (valor_a == valor_b):
+    print('Valores iguais.')
+
+valor = 2
+if (valor % 2 == 0):
+    print(f'O número {valor} é par!')
+else:
+    print(f'O número {valor} é impar!')
