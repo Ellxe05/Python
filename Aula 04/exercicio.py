aad0e15a2a60c0937813a14d7ad06d08e28eeba1
Gabrielle <142362674+Ellxe05@users.noreply.github.com>
2024-05-08 14:15:11 -0300

    Delete exercicio.py

--- Aula 04/exercicio.py
+++
@@ -1,9 +0,0 @@
-inicial = int(input('Qual valor deseja iniciar a contagem? '))
-final = int(input('Qual valor deseja encerrar a contagem? '))
-
-x = inicial
-while (x <= final):
-    #Verifica se o número é par
-    if (x % 2 == 0):
-        print(x)
-    x = x + 1
