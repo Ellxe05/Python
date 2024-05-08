1070564905acd24a8df56e03156f66bbce1e495e
Gabrielle <142362674+Ellxe05@users.noreply.github.com>
2024-05-08 14:54:59 -0300

    Update estrutura-repeticao-aninhada.py

--- Aula 04/estrutura-repeticao-aninhada.py
+++ Aula 04/estrutura-repeticao-aninhada.py
@@ -1,9 +1,16 @@
 #2x while
-tabuada = 1
-while tabuada <= 10:
+# tabuada = 1
+# while tabuada <= 10:
+#     print(f'TABUADA DO {tabuada}:')
+#     i = 1
+#     while i <= 10:
+#         print(f'{tabuada} x {i} = {tabuada * i}')
+#         i += 1
+#     tabuada += 1
+
+#2x for
+for tabuada in range(1,11,1):
     print(f'TABUADA DO {tabuada}:')
-    i = 1
-    while i <= 10:
+    for i in range(1,11,1):
         print(f'{tabuada} x {i} = {tabuada * i}')
-        i += 1
-    tabuada += 1
+
