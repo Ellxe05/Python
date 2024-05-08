12c8ac72b137eb24cfe744b5596efd9d1e6ed974
Gabrielle <142362674+Ellxe05@users.noreply.github.com>
2024-05-08 16:12:28 -0300

    Update trabalho-02.py

---
+++ TRABALHO 01/trabalho-02.py
@@ -0,0 +1,8 @@
+print('Bem-vindo a Loja de Gelados da Gabrielle Silva')
+print('-----------------Cardápio-----------------')
+print('--| Tamanho | Cupuaçu (CP) | Açaí (AC) |--')
+print('--|    P    |   R$  9.00   | R$ 11.00  |--')
+print('--|    M    |   R$  14.00  | R$ 16.00  |--')
+print('--|    G    |   R$  18.00  | R$ 20.00  |--')
+print('------------------------------------------')
+
