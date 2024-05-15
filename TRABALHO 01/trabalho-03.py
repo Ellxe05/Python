718f3b639f8e845052069b49fab68cac38e5c87e
Gabrielle <142362674+Ellxe05@users.noreply.github.com>
2024-05-15 11:50:30 -0300

    questao 03

--- TRABALHO 01/trabalho-03.py
+++ TRABALHO 01/trabalho-03.py
@@ -1,1 +1,8 @@
 #QUESTÃO 03
+print('Bem vindo a Copiadora da Gabrielle Silva')
+print('Entre com o tipo de serviço desejado:')
+print('DIG - Digitalização')
+print('ICO - Impressão Colorida')
+print('IPB - Impressão Preto e Branco')
+print('FOT - Fotocópia')
+
