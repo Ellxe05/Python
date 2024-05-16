55422d21b572bbf0b509c042102c4500513e7794
Gabrielle <142362674+Ellxe05@users.noreply.github.com>
2024-05-16 12:13:03 -0300

    Update trabalho-04.py

--- TRABALHO 01/trabalho-04.py
+++ TRABALHO 01/trabalho-04.py
@@ -1,1 +1,13 @@
 #QUESTAO 04
+print('Bem vindo a Livraria da Gabrielle Silva')
+print('-' * 50)
+print('                MENU PRINCIPAL')
+print('-' * 50)
+
+print('Escolha a opção desejada:')
+print('1 - Cadastrar Livro')
+print('2 - Consultar Livro(s)')
+print('3 - Remover Livro')
+print('4 - Sair')
+
+
