3efb736689884ea02b19b69855dddde5f3eb383d
Gabrielle <142362674+Ellxe05@users.noreply.github.com>
2024-05-16 15:01:19 -0300

    Update trabalho-04.py

--- TRABALHO 01/trabalho-04.py
+++ TRABALHO 01/trabalho-04.py
@@ -4,36 +4,39 @@ print('Bem vindo a Livraria da Gabrielle Silva')
 lista_livro = []
 id_global = 0
 
-def cadastrar_livro(id):
+def cadastrar_livro():
+    global id_global
+    id_global += 1
     nome = input('Por favor entre com o nome do livro: ')
     autor = input('Por favor entre com o autor do livro: ')
     editora = input('Por favor entre com a editora do livro: ')
-    livro = {'id': id, 'nome': nome, 'autor': autor, 'editora': editora}
+    livro = {'id': id_global, 'nome': nome, 'autor': autor, 'editora': editora}
     lista_livro.append(livro)
 
 def consultar_livro():
-    opcao = input('1 - Consultar Todos | 2 - Consultar por Id | 3 - Consultar por Autor | 4 - Retornar ao menu: ')
-    if (opcao == '1'):
-        for livro in lista_livro:
-            print(livro)
-    elif (opcao == '2'):
-        consultando_id = input('Digite o id do livro: ')
-        for livro in lista_livro:
-            if (livro['id'] == consultando_id):
+    while True:
+        opcao = input('1 - Consultar Todos | 2 - Consultar por Id | 3 - Consultar por Autor | 4 - Retornar ao menu: ')
+        if (opcao == '1'):
+            for livro in lista_livro:
                 print(livro)
-                break
-    elif (opcao == '3'):
-        consultando_autor = input('Digite o autor do(s) livro(s): ')
-        for livro in lista_livro:
-            if (livro['autor'] == consultando_autor):
-                print(livro)
-    elif (opcao == '4'):
-        return
-    else:
-        print('Opção inválida')
+        elif (opcao == '2'):
+            consultando_id = int(input('Digite o id do livro: '))
+            for livro in lista_livro:
+                if (livro['id'] == consultando_id):
+                    print(livro)
+                    break
+        elif (opcao == '3'):
+            consultando_autor = input('Digite o autor do(s) livro(s): ')
+            for livro in lista_livro:
+                if (livro['autor'] == consultando_autor):
+                    print(livro)
+        elif (opcao == '4'):
+            return
+        else:
+            print('Opção inválida')
 
 def remover_livro():
-    remocao = input('Digite o id do livro a ser removido: ')
+    remocao = int(input('Digite o id do livro a ser removido: '))
     for livro in lista_livro:
         if (livro['id'] == remocao):
             lista_livro.remove(livro)
@@ -54,7 +57,10 @@ while True:
     
     opcao_escolhida = input('>>')
     if (opcao_escolhida == '1'):
-        cadastrar_livro(id_global)
+        print('-' * 50)
+        print('                MENU CADASTRAR LIVRO')
+        print('-' * 50)
+        cadastrar_livro()
     elif (opcao_escolhida == '2'):
         print('-' * 50)
         print('                MENU CONSULTAR LIVRO')
